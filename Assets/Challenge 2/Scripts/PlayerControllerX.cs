54fc5435834ed8ec0147a15f01845f87aad46992
Chad Linthicum <portableanswers@gmail.com>
2022-12-07 13:38:21 -0500

    First commit. Game is complete for now.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    // Floats for preventing space spam
+    private float spawnTimeInterval = 1; // Interval between dog spawns
+    private float timerX; // Timer for the interval
+
+
+    private void Start()
+    {
+        // For the first spawn, timer is 1 which means we can spawn a dog
+        timerX = spawnTimeInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This starts the timer
+        timerX += Time.deltaTime;
+        // If space is down and timer is equal or bigger than 1 we can spawn a dog
+        if (Input.GetKeyDown(KeyCode.Space) && timerX >= spawnTimeInterval)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // Resets timer back to 0
+            timerX = 0;
+        }
+
+    }
+}
